20b28869eedde13dbceafd16e02a8057dde8a08a
Pranaya Widi Ramadhan <pranayadiscord@gmail.com>
2026-08-18 12:10:11 +1000

    backend-sonar

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'sql105.infinityfree.com',
   user: process.env.DB_USER || 'if0_42681185',
-  password: process.env.DB_PASSWORD || '5773adxTYN',
+  password: process.env.DB_PASSWORD || '5773adxTYNk',
   database: process.env.DB_NAME || 'if0_42681185_sonar_db',
   port: parseInt(process.env.DB_PORT || '3306'),
   waitForConnections: true,
